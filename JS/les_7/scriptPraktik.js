408a9e28baf12fc7347cfa1a3163444bc218b511
Drekrotion <Ramasik@gmail.com>
2019-09-03 21:25:45 +0300

    Add new Task

---
+++ JS/les_7/scriptPraktik.js
@@ -0,0 +1,27 @@
+
+
+
+/*
+function fact(num, cb) {
+    if (num !== 1) {
+        return num * cb(num -1, cb)
+    } else {
+        return 1
+    }
+}
+
+
+
+let sum = fact(5, fact);
+
+console.log(sum);
+*/
+
+
+
+
+
+
+
+
+
