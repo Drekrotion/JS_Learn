eb0c0e348e1ffd406ead1d4c7f1c829bda680474
Drekrotion <Ramasik@gmail.com>
2019-09-03 19:48:48 +0300

    Add new Task

---
+++ JS/les_7/script.js
@@ -0,0 +1,25 @@
+let arr = [32,46,357,[999,888,[1111100],777,666,555],468,324,131,4];
+
+let i = 0;
+
+function see() {
+    if (i < arr.length){
+        if (Array.isArray(arr[i])) {
+            arr.splice(3,1, ...arr[i])
+        } if (Array.isArray(arr[i])) {
+            arr.splice(5,1, ...arr[i])
+        }
+        console.log(arr[i]);
+        ++i;
+        see()
+    }
+
+}
+
+see();
+
+
+
+
+
+
