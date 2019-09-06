f43dc2b485e64f51fffb2bae9ea58c9ba007c714
Drekrotion <Ramasik@gmail.com>
2019-09-06 21:30:32 +0300

    Add new Task

--- JS/les_7/scriptPraktik.js
+++ JS/les_7/scriptPraktik.js
@@ -19,9 +19,62 @@ console.log(sum);
 
 
 
+// function fibonacci(max) {
+//     let result = [0, 1];
+//
+//     for (let i = 1; result[i] < max; i++) {
+//         result.push(result[i] + result[i - 1]);
+//     }
+//
+//     console.log(result);
+//
+// }
+//
+//
+// fibonacci(10);
+//
 
 
 
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function fib(max) {
+    let result = [0,1];
+
+    for (let i = 1; result[i] < max; i++ ){
+        result.push(result[i] + result[i - 1])
+    }
+
+    console.log(result);
+
+}
+
+fib(9);
