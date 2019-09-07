14213365d5d8ddde09baeff61eb164df3e5e22be
Drekrotion <Ramasik@gmail.com>
2019-09-07 17:20:15 +0300

    Add new Task

--- JS/les_8/popelushka.js
+++ JS/les_8/popelushka.js
@@ -1,17 +1,15 @@
-
-class Popelushka  {
-    constructor (name, age, size) {
+class Popelushka {
+    constructor(name, age, size) {
         this.name = name;
         this.age = age;
         this.size = size;
     }
 
-    info(){
+    info() {
         console.log(this);
     }
 
 }
-
 
 
 let Oksana = new Popelushka('Oksana', 23, 38);
@@ -21,41 +19,39 @@ let Vika = new Popelushka('Vika', 18, 40);
 let Natalia = new Popelushka('Natalia', 25, 36);
 
 
-
-
 class Prinss {
-    constructor (name, age, tyfla){
+    constructor(name, age, tyfla) {
         this.name = name;
         this.age = age;
         this.tyfla = tyfla;
     }
-    info(){
+
+    info() {
         console.log(this);
     }
 }
 
 let Roman = new Prinss('Roman', 23, 38);
-let Taras= new Prinss('Taras', 28, 32);
+let Taras = new Prinss('Taras', 28, 32);
 let Dimas = new Prinss('Dimas', 18, 35);
 let Nazar = new Prinss('Nazar', 31, 42);
 let Viktor = new Prinss('Viktor', 23, 36.5);
 
 
-
-let arrPopelushka = [Oksana,Anna,Solomia,Vika,Natalia];
-let arrPrinss = [Roman,Taras,Dimas,Nazar,Viktor];
-
+let arrPopelushka = [Oksana, Anna, Solomia, Vika, Natalia];
+let arrPrinss = [Roman, Taras, Dimas, Nazar, Viktor];
 
 
 let one = [];
 
 
-arrPrinss.forEach(() =>{
+arrPrinss.forEach((arrTyfla) => {
 
-    
 
 
 });
+
+
 
 
 
