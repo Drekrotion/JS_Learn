4318e9770ed350481464dec764ad9ca17e57e85d
Drekrotion <Ramasik@gmail.com>
2019-10-01 23:00:09 +0300

    NodeJS new Task - Les1 (dz)

---
+++ nodeJS/les_1/DZ/index.js
@@ -0,0 +1,37 @@
+const fs = require('fs');
+
+
+exports.criatedDir = function (NameDir) {
+    fs.mkdir(`./nodeJS/les_1/DZ/${NameDir}`, err => {
+        if (!err){
+            console.log('ok');
+        }
+    })
+};
+
+
+exports.createdStud = function (whereFold, nameFoldersStudents, nameFile, dateStudents) {
+    fs.mkdir(`./nodeJS/les_1/DZ/${whereFold}/${nameFoldersStudents}`, err => {
+        if (!err) {
+            console.log('createdStudFold!!!!!!!!!!!!!!!!!!!');
+        }
+    });
+
+    setTimeout(()=> {
+        let writeStreamFile = fs.createWriteStream(`./nodeJS/les_1/DZ/${whereFold}/${nameFoldersStudents}/${nameFile}.txt`);
+        writeStreamFile.write(JSON.stringify(dateStudents))
+    },0);
+
+};
+
+
+
+
+
+
+
+
+
+
+
+
